3c62e86eb36d3ab0d209b143fe2eb29f1edcfbf4
viveoff <vlad.shorodok14@gmail.com>
2024-09-14 21:48:34 +0200

    Created Auth Controller

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { sign } from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get
+router.get("/signup",  signup);
+router.get("/login", login);
+router.get("/logout", logout);
+
 
 export default router;
